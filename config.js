5ca95994bff2ca44f698b80ad7b552399b9c0cd4
Mayank Singh <mayank.singh@bacancy.com>
2025-02-25 18:36:27 +0530

    changed env variables naming convention

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ import getConfig from 'next/config'
 
 const { publicRuntimeConfig } = getConfig()
 
-export const API = publicRuntimeConfig.API
-export const APP_NAME = publicRuntimeConfig.APP_NAME
-export const DOMAIN = publicRuntimeConfig.DOMAIN
-export const PRODUCTION = publicRuntimeConfig.PRODUCTION
-export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID
+export const API = publicRuntimeConfig.REACT_APP_API
+export const APP_NAME = publicRuntimeConfig.REACT_APP_APP_NAME
+export const DOMAIN = publicRuntimeConfig.REACT_APP_DOMAIN
+export const PRODUCTION = publicRuntimeConfig.REACT_APP_PRODUCTION
+export const FB_APP_ID = publicRuntimeConfig.REACT_APP_FB_APP_ID
